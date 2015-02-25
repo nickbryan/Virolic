7815bf4bbf36ac65317dcb9bf141539eda1bf394
Nick Bryan <nickbryan.music@gmail.com>
2015-02-25 20:57:40 +0000

    vector improvements

--- engine/src/math/vector2d.js
+++ engine/src/math/vector2d.js
@@ -1,23 +1,83 @@
 (function() {
     opus.vector2d = Object.extend({
 
-       init: function(x, y) {
+        init: function(x, y) {
             return this.set(x || 0, y || 0);
-       },
+        },
 
-       set: function(x, y) {
-           if (isNaN(x)) {
-               throw new Error("Invalid x parameter for vector");
-           }
-           if (isNaN(y)) {
-               throw new Error("Invalid y parameter for vector");
-           }
+        set: function(x, y) {
+            if (isNaN(x)) {
+                throw new Error("Invalid x parameter for vector");
+            }
+            if (isNaN(y)) {
+                throw new Error("Invalid y parameter for vector");
+            }
 
-           this.x = x;
+            this.x = x;
 
-           this.y = y;
+            this.y = y;
 
-           return this;
-       }
+            return this;
+        },
+
+        add: function(vector) {
+            return new opus.vector2d(this.x + vector.x, this.y + vector.y);
+        },
+
+        addTo: function(vector) {
+            this.x += vector.x;
+            this.y += vector.y;
+            return this;
+        },
+
+        subtract: function(vector) {
+            return new opus.vector2d(this.x - vector.x, this.y - vector.y);
+        },
+
+        subtractFrom: function(vector) {
+            this.x -= vector.x;
+            this.y -= vector.y;
+            return this;
+        },
+
+        multiplyScalar: function(value) {
+            return return new opus.vector2d(this.x * value, this.y * value);
+        },
+
+        multiplyByScalar: function(value) {
+            this.x * value;
+            this.y * value;
+            return this;
+        },
+
+        divideScalar: function(value) {
+            return return new opus.vector2d(this.x / value, this.y / value);
+        },
+
+        divideByScalar: function(value) {
+            this.x / value;
+            this.y / value;
+            return this;
+        },
+
+        setAngle: function(angle) {
+            var length = this.getLength();
+            this.x = Math.cos(angle) * length;
+            this.y = Math.sin(angle) * length;
+        },
+
+        getAngle: function() {
+            return Math.atan2(this.y, this.x);
+        },
+
+        setLength: function(length) {
+            var angle = this.getAngle();
+            this.x = Math.cos(angle) * length;
+            this.y = Math.sin(angle) * length;
+        },
+
+        getLength: function() {
+            return Math.sqrt((this.x * this.x) + (this.y * this.y));
+        }
     });
 })();
